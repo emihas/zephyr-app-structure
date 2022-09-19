6f270c2d0357adca1884b1d012324a007d0ba8f9
emihas <emihas@addiva.se>
2022-09-19 10:00:28 +0200

    Add nrf5340 board definitions

--- boards/arm/custom_plank/board.cmake
+++ boards/arm/custom_plank/board.cmake
@@ -1,10 +1,10 @@
 # Copyright (c) 2021 Nordic Semiconductor ASA
 # SPDX-License-Identifier: Apache-2.0
 
-board_runner_args(jlink "--device=nrf52" "--speed=4000")
-board_runner_args(pyocd "--target=nrf52840" "--frequency=4000000")
+board_runner_args(jlink "--device=nrf53" "--speed=4000")
+board_runner_args(pyocd "--target=nrf5340" "--frequency=4000000")
 
-set(OPENOCD_NRF5_SUBFAMILY "nrf52")
+set(OPENOCD_NRF5_SUBFAMILY "nrf53")
 
 include(${ZEPHYR_BASE}/boards/common/nrfjprog.board.cmake)
 include(${ZEPHYR_BASE}/boards/common/jlink.board.cmake)
